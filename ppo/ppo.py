04afc8059b8f6a407ccd94e9543e6dc6c3173619
blessing <blessing.denis@gmx.de>
2020-11-06 23:23:20 +0100

    added collect_rollout fn

--- ppo/ppo.py
+++ ppo/ppo.py
@@ -1,8 +1,45 @@
+import numpy as np
+
 class PPO:
     def __init__(self,
+                 actor,
+                 critic,
+                 env,
+                 replay_buffer,
                  learning_rate,
                  batch_size,
                  gamma,
                  max_grad_norm,
                  device):
-        ...
+        self.actor = actor
+        self.critic = critic
+        self.env = env
+        self.replay_buffer = replay_buffer
+        self.learning_rate = learning_rate
+        self.batch_size = batch_size
+        self.gamma = gamma
+        self.max_grad_norm = max_grad_norm
+        self.device = device
+
+    def collect_rollouts(self, n_episodes):
+        self.replay_buffer.reset()
+        for i in range(n_episodes):
+            last_obs = self.env.reset()
+            last_done = False
+            while not last_done:
+                action = self.actor(...)
+                log_prob = self.actor(...)
+                value = self.critic(...)
+                obs, reward, done, _ = self.env.step(action)
+                self.replay_buffer.push(last_obs, action, reward, last_done, value, log_prob)
+                last_obs, last_done = obs, done
+
+    def calc_return_and_advantage(self):
+        rewards, values = self.replay_buffer.rewards, self.replay_buffer.values
+        # Not sure here: What is the return in the last time step?
+        expected_return = rewards + self.gamma * np.concatenate([values[1:], values[-1]])
+        advantage = expected_return - values
+        return expected_return, advantage
+
+
+
--- ppo/rollout_buffer.py
+++ ppo/rollout_buffer.py
@@ -3,10 +3,15 @@ import numpy as np
 
 class RolloutBuffer:
     def __init__(self,
+                 actor,
+                 critic,
                  buffer_size,
                  n_obs,
                  n_actions,
                  ):
+
+        self.actor = actor
+        self.critic = critic
         self.buffer_size = buffer_size
         self.n_obs = n_obs
         self.n_actions = n_actions
@@ -14,16 +19,27 @@ class RolloutBuffer:
 
         self.observations, self.actions, self.rewards, self.advantages = None, None, None, None
         self.returns, self.dones, self.values, self.log_probs = None, None, None, None
+        self.pos = 0
+        self.full = False
         self.reset()
 
     def reset(self) -> None:
-        self.actions = np.zeros((self.buffer_size, self.n_actions), dtype=np.float32)
-        self.rewards = np.zeros(self.buffer_size, dtype=np.float32)
-        self.returns = np.zeros(self.buffer_size, dtype=np.float32)
-        self.dones = np.zeros(self.buffer_size, dtype=np.float32)
-        self.values = np.zeros(self.buffer_size, dtype=np.float32)
-        self.log_probs = np.zeros(self.buffer_size, dtype=np.float32)
-        self.advantages = np.zeros(self.buffer_size, dtype=np.float32)
+        self.pos = 0
+        self.actions = ()
+        self.rewards = ()
+        self.returns = ()
+        self.dones = ()
+        self.values = ()
+        self.log_probs = ()
+        self.advantages = ()
 
-    def push(self):
-        ...  # todo
+    def push(self, obs, action, reward, done, value, log_prob):
+        self.observations.append(np.array(obs).copy()).append(np.array(action).copy())
+        self.rewards.append(np.array(reward).copy())
+        self.dones.append(np.array(done).copy())
+        self.values.append(value.clone().cpu().numpy().flatten())
+        self.log_probs.append(log_prob.clone().cpu().numpy())
+
+
+
+
