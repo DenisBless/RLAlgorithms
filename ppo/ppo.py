3c4d2061e2e76d9273e5d4d8e731ac4e58d15649
blessing <blessing.denis@gmx.de>
2020-11-06 23:47:23 +0100

    added advantage and return calculations

--- ppo/ppo.py
+++ ppo/ppo.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+
 class PPO:
     def __init__(self,
                  actor,
@@ -7,6 +8,7 @@ class PPO:
                  env,
                  replay_buffer,
                  learning_rate,
+                 n_epochs,
                  batch_size,
                  gamma,
                  max_grad_norm,
@@ -16,6 +18,8 @@ class PPO:
         self.env = env
         self.replay_buffer = replay_buffer
         self.learning_rate = learning_rate
+        self.n_epochs = n_epochs
+
         self.batch_size = batch_size
         self.gamma = gamma
         self.max_grad_norm = max_grad_norm
@@ -31,15 +35,20 @@ class PPO:
                 log_prob = self.actor(...)
                 value = self.critic(...)
                 obs, reward, done, _ = self.env.step(action)
-                self.replay_buffer.push(last_obs, action, reward, last_done, value, log_prob)
+                self.replay_buffer.add_transition_vars(last_obs, action, reward, last_done, value, log_prob)
                 last_obs, last_done = obs, done
+            returns, advantages = self.calc_return_and_advantage()
+            self.replay_buffer.add_episode_vars(returns, advantages)
 
     def calc_return_and_advantage(self):
+        #Todo No GAE implemented yet
         rewards, values = self.replay_buffer.rewards, self.replay_buffer.values
         # Not sure here: What is the return in the last time step?
         expected_return = rewards + self.gamma * np.concatenate([values[1:], values[-1]])
         advantage = expected_return - values
         return expected_return, advantage
 
+    def train(self):
+        self.replay_buffer.to_numpy()
 
-
+        for epoch in range(self.n_epochs)
--- ppo/rollout_buffer.py
+++ ppo/rollout_buffer.py
@@ -3,19 +3,11 @@ import numpy as np
 
 class RolloutBuffer:
     def __init__(self,
-                 actor,
-                 critic,
-                 buffer_size,
                  n_obs,
                  n_actions,
                  ):
-
-        self.actor = actor
-        self.critic = critic
-        self.buffer_size = buffer_size
         self.n_obs = n_obs
         self.n_actions = n_actions
-        self.observations = np.zeros(self.buffer_size + self.n_obs, dtype=np.float32)
 
         self.observations, self.actions, self.rewards, self.advantages = None, None, None, None
         self.returns, self.dones, self.values, self.log_probs = None, None, None, None
@@ -24,22 +16,44 @@ class RolloutBuffer:
         self.reset()
 
     def reset(self) -> None:
+        """
+        Reset the replay buffer.
+        """
         self.pos = 0
         self.actions = ()
         self.rewards = ()
-        self.returns = ()
         self.dones = ()
         self.values = ()
         self.log_probs = ()
-        self.advantages = ()
+        self.returns = None
+        self.advantages = None
 
-    def push(self, obs, action, reward, done, value, log_prob):
+    def add_transition_vars(self, obs, action, reward, done, value, log_prob) -> None:
         self.observations.append(np.array(obs).copy()).append(np.array(action).copy())
         self.rewards.append(np.array(reward).copy())
         self.dones.append(np.array(done).copy())
         self.values.append(value.clone().cpu().numpy().flatten())
         self.log_probs.append(log_prob.clone().cpu().numpy())
 
+    def add_episode_vars(self, returns, advantages) -> None:
+        self.returns = returns
+        self.advantages = advantages
 
+    def to_numpy(self) -> None:
+        """
+        Transforms the rollout lists to numpy arrays for sampling
+        """
+        self.actions = np.array(self.actions, dtype=np.float32)
+        self.rewards = np.array(self.rewards, dtype=np.float32)
+        self.dones = np.array(self.dones, dtype=np.float32)
+        self.values = np.array(self.values, dtype=np.float32)
+        self.log_probs = np.array(self.log_probs, dtype=np.float32)
 
+    def sample(self):
+        """
+        Create a generator which uses a random permutation of the indices for the elements of the rollout buffer.
+        Therefore, calling sample() returns each datapoint from the rollout buffer once, but in random order.
 
+        :return:
+        """
+        ...
