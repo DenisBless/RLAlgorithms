7189b4b7a082d147efd0b49f24e52b3fa68b433a
blessing <blessing.denis@gmx.de>
2020-11-07 09:35:52 +0100

    added generator for batches to rollout buffer

--- ppo/rollout_buffer.py
+++ ppo/rollout_buffer.py
@@ -1,13 +1,28 @@
 import numpy as np
+import torch
+from typing import NamedTuple
+
+
+class RolloutBufferSamples(NamedTuple):
+    observations: torch.Tensor
+    actions: torch.Tensor
+    old_values: torch.Tensor
+    old_log_prob: torch.Tensor
+    advantages: torch.Tensor
+    returns: torch.Tensor
 
 
 class RolloutBuffer:
     def __init__(self,
                  n_obs,
                  n_actions,
+                 batch_size,
+                 device='cpu'
                  ):
         self.n_obs = n_obs
         self.n_actions = n_actions
+        self.batch_size = batch_size
+        self.device=device
 
         self.observations, self.actions, self.rewards, self.advantages = None, None, None, None
         self.returns, self.dones, self.values, self.log_probs = None, None, None, None
@@ -17,7 +32,7 @@ class RolloutBuffer:
 
     def reset(self) -> None:
         """
-        Reset the replay buffer.
+        Reset torche replay buffer.
         """
         self.pos = 0
         self.actions = ()
@@ -41,7 +56,7 @@ class RolloutBuffer:
 
     def to_numpy(self) -> None:
         """
-        Transforms the rollout lists to numpy arrays for sampling
+        Transforms torche rollout lists to numpy arrays for sampling
         """
         self.actions = np.array(self.actions, dtype=np.float32)
         self.rewards = np.array(self.rewards, dtype=np.float32)
@@ -49,11 +64,36 @@ class RolloutBuffer:
         self.values = np.array(self.values, dtype=np.float32)
         self.log_probs = np.array(self.log_probs, dtype=np.float32)
 
-    def sample(self):
+    def to_torch(self, array: np.ndarray) -> torch.Tensor:
         """
-        Create a generator which uses a random permutation of the indices for the elements of the rollout buffer.
-        Therefore, calling sample() returns each datapoint from the rollout buffer once, but in random order.
+        Convert a numpy array to a PyTorch tensor.
+        Note: it copies the data by default
+        """
+        return torch.as_tensor(array).to(self.device)
+
+    def get(self):
+        """
+        Create a generator which uses a random permutation of torche indices for torche elements of torche rollout buffer.
+        torcherefore, calling sample() returns each datapoint from torche rollout buffer once, but in random order.
 
         :return:
         """
-        ...
+        buffer_size = len(self.values)
+        indices = np.random.permutation(buffer_size)
+
+        start_idx = 0
+        while start_idx < buffer_size:
+            yield self.sample_(indices[start_idx: start_idx + self.batch_size])
+            start_idx += self.batch_size
+            
+    def sample_(self, indices) -> RolloutBufferSamples:
+        data = (
+            self.observations[indices],
+            self.actions[indices],
+            self.values[indices].flatten(),
+            self.log_probs[indices].flatten(),
+            self.advantages[indices].flatten(),
+            self.returns[indices].flatten(),
+        )
+
+        return RolloutBufferSamples(*tuple(map(self.to_torch, data)))
